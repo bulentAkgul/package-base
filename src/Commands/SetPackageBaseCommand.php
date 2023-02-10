a42d66da6ec1f03b77279464238177a18260ebc7
Bulent Akgul <bulent.akgul@me.com>
2023-02-10 12:40:55 +0300

    composer.json creator fixed

--- src/Commands/SetPackageBaseCommand.php
+++ src/Commands/SetPackageBaseCommand.php
@@ -9,8 +9,12 @@ class SetPackageBaseCommand extends Command
     protected $signature = 'set-package-base';
     protected $description = '';
 
+    private $stubs;
+
     public function handle()
     {
+        $this->stubs();
+
         $this->composer();
 
         $this->copy();
@@ -18,6 +22,21 @@ class SetPackageBaseCommand extends Command
         $this->env();
 
         $this->addPackages();
+    }
+
+    private function stubs(): void
+    {
+        $this->stubs = implode(
+            DIRECTORY_SEPARATOR,
+            [__DIR__, '..', '..', 'stubs']
+        );
+    }
+
+    private function copy(): void
+    {
+        copy("{$this->stubs}/phpunit.stub", base_path('phpunit.xml'));
+        copy("{$this->stubs}/vite.stub", base_path('vite.config.js'));
+        copy("{$this->stubs}/test-case.stub", base_path('package/tests/TestCase.php'));
     }
 
     private function composer(): void
@@ -30,31 +49,54 @@ class SetPackageBaseCommand extends Command
 
     private function composerContent(): string
     {
-        return str_replace(
-            '{{ package }}',
-            $this->packageName(),
-            file_get_contents(base_path('composer.json'))
-        );
+        return str_replace('\/', '/', json_encode(
+            $this->setComposerContent(),
+            JSON_PRETTY_PRINT
+        ));
+    }
+
+    private function setComposerContent(): array
+    {
+        $package = $this->packageName();
+
+        $composer = [];
+
+        foreach (json_decode(file_get_contents(
+            base_path('composer.json')
+        ), true) as $key => $value) {
+            $composer[$key] = $value;
+
+            if ($key == 'license') {
+                $composer['repositories'] = [
+                    $package => [
+                        'type' => 'path',
+                        'url' => 'package',
+                        'options' => [
+                            'symlink' => true
+                        ]
+                    ]
+                ];
+            }
+
+            if ($key == 'require-dev') {
+                $composer[$key]["bakgul/{$package}"] = '@dev';
+
+                ksort($composer[$key]);
+            }
+        }
+
+        return $composer;
     }
 
     private function packageName(): string
     {
         foreach (file(base_path('package/composer.json')) as $line) {
             if (str_contains($line, '"name": "bakgul')) {
-                return str_replace(['"', ','], '', explode('/', $line)[1]);
+                return trim(str_replace(['"', ','], '', explode('/', $line)[1]));
             }
         }
 
         return 'package-name';
-    }
-
-    private function copy(): void
-    {
-        $base  = __DIR__ . '/../..';
-
-        copy("{$base}/stubs/phpunit.stub", base_path('phpunit.xml'));
-        copy("{$base}/stubs/vite.stub", base_path('vite.config.js'));
-        copy("{$base}/stubs/test-case.stub", base_path('package/tests/TestCase.php'));
     }
 
     private function env(): void
